1df41c6b85220fcaecce31e4a12dec2272607ebb
vgrowhouse999 <127393734+vgrowhouse999@users.noreply.github.com>
2024-09-08 01:38:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,72 @@
-function getRandomElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
+(() => {
+  const container = document.getElementById("container");
 
-const artStyles = [/* list of art styles */];
-const cars = [/* list of cars */];
-const colors = [/* list of colors */];
-const mods = [/* list of mods */];
+  const hexagons = container.querySelectorAll(".hexagon");
+  const hexagonElements = new Array(...hexagons);
 
-function update() {
-    const artStyle = getRandomElement(artStyles);
-    const car = getRandomElement(cars);
-    const color = getRandomElement(colors);
-    const mod = getRandomElement(mods);
+  const ripple = (target) => {
+    if (container.classList.contains("show-ripple")) {
+      return;
+    }
+    const targetRect = target.getBoundingClientRect();
+    const data = hexagonElements
+      .map((element) => {
+        const rect = element.getBoundingClientRect();
+        const centerX = rect.x + rect.width / 2;
+        const centerY = rect.y + rect.height / 2;
+        const distance = Math.round(
+          Math.sqrt(
+            Math.pow(rect.x - targetRect.x, 2) +
+              Math.pow(rect.y - targetRect.y, 2)
+          )
+        );
+        return { element, rect, centerX, centerY, distance };
+      })
+      .sort((a, b) =>
+        a.distance > b.distance ? 1 : a.distance < b.distance ? -1 : 0
+      );
 
-    const output = `The scene portrays the car, a ${car} painted in ${color} color to the tone of ${artStyle}. Car Mods are: ${mod}.`;
-    
-    document.getElementById('output').innerText = output;
-}
+    const [max] = data.slice(-1);
+    data.forEach((item) =>
+      item.element.style.setProperty(
+        "--ripple-factor",
+        `${(item.distance * 100) / max.distance}`
+      )
+    );
+    container.classList.toggle("show-ripple");
+    const cleanUp = () => {
+      requestAnimationFrame(() => {
+        container.classList.remove("show-ripple");
+        data.forEach((item) =>
+          item.element.style.removeProperty("--ripple-factor")
+        );
+        max.element.removeEventListener("animationend", cleanUp);
+      });
+    };
+    max.element.addEventListener("animationend", cleanUp);
+  };
+
+  hexagons.forEach((hexagon) => {
+    hexagon.addEventListener("click", () => {
+      ripple(hexagon, hexagons);
+    });
+  });
+
+   const switchButton = document.getElementById('switch');
+    const toggleTheme = () => {
+        switchButton.classList.toggle('checked');
+        document.documentElement.classList.toggle('vision-ui');
+    };
+    switchButton.addEventListener('click', toggleTheme);
+
+    // demo
+    setTimeout(() => {
+    ripple(hexagonElements[0], hexagons);
+    setTimeout(() => {
+      toggleTheme();
+      setTimeout(() => {
+        ripple(hexagonElements[0], hexagons);
+      }, 600);
+    }, 900);
+  }, 300);
+})();
